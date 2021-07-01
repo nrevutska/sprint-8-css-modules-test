84ac49c6ad1d019e21aef58e773176d9b99bf0d0
nrevutska <46501186+nrevutska@users.noreply.github.com>
2021-07-01 21:15:22 +0300

    Update Button.e2e.test.js

--- Button.e2e.test.js
+++ Button.e2e.test.js
@@ -2,31 +2,31 @@ import puppeteer from 'puppeteer';
 
 let browser, page;
 
-// beforeAll(async () => {  
-//   browser = await puppeteer.launch({
-//     args: [
-//       '--no-sandbox',
-//       '--disable-setuid-sandbox',
-//       '--disable-gpu'
-//     ],
-//     timeout: 10000,
-//     headless: true,
-//     slowMo: 30
-//   });
-//   page = await browser.newPage();
-//   await page.goto('http://localhost:3000/', {waitUntil: 'load'});
-// }, 120000);
+beforeAll(async () => {  
+  browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu'
+    ],
+    timeout: 10000,
+    headless: true,
+    slowMo: 30
+  });
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000/', {waitUntil: 'load'});
+}, 120000);
 
-test.skip('Button has correct color', async () => {
+test('Button has correct color', async () => {
   const color = await page.$eval('button', el => window.getComputedStyle(el).backgroundColor);
   expect(color).toBe('rgb(173, 216, 230)'); // lightblue
 });
 
-test.skip('Input has correct color', async () => {
+test('Input has correct color', async () => {
   const color = await page.$eval('input', el => window.getComputedStyle(el).backgroundColor);
   expect(color).toBe('rgb(240, 255, 255)'); // azure
 });
 
-// afterAll(async () => {  
-//   await browser.close()
-// });
+afterAll(async () => {  
+  await browser.close()
+});
