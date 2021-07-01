279756457bfe94e8e84ef56fc81712c60212edd3
nrevutska <46501186+nrevutska@users.noreply.github.com>
2021-07-01 17:04:35 +0300

    Create Input.test.js

---
+++ Input.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Input from '../Input/Input';
+
+test('Renders with a className equal to \'active\'', () => {
+  const { container } = render(<Input />)
+  expect(container.firstChild).toHaveClass('active') 
+})
