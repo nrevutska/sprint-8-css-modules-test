7103d18dec3a447cdff40d557650f3ba9c0d7b67
nrevutska <46501186+nrevutska@users.noreply.github.com>
2021-08-11 22:33:12 +0300

    Update Input.test.js

--- Input.test.js
+++ Input.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Input from '../Input/Input';
+import Input from '../Input';
 
 test('Renders with a className equal to \'active\'', () => {
   const { container } = render(<Input />)
