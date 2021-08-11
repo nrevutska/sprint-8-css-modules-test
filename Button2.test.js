386df37f8570b8b1dccec1638685d15523050b0b
nrevutska <46501186+nrevutska@users.noreply.github.com>
2021-08-11 22:32:23 +0300

    Update Button2.test.js

--- Button2.test.js
+++ Button2.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Button from '../Button/Button';
+import Button from '../Button';
 
 
 test('Renders with a className equal to the active', () => {
